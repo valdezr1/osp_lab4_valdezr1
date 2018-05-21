be8df2a0dc2aaa6c448894ec7b140fded6c815e2
Valdez <valdezr1@debian-9.3.0-template>
2018-05-21 15:17:32 -0700

    Adding validation for <starting directory> to ensure appropriate placements of slashes

--- file_search.c
+++ file_search.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 
 int main(int argc, char** argv){
+	
+	if(argc == 3){
+		printf("Arguments count vaild\n");
+		
+		//Ensuring that the <starting directory> begins with a '/' and doesn't end with a '/' 
+		if (argv[2][0] != '/' || argv[2][strlen(argv[2]) - 1] == '/'){
+			printf("Starting Directory Argument is invalid\n");
+			return 0;
+		}
+	}
+	else{
+		printf("Invalid Number of Arguments \n");
+	}
 
 	return 0;
 
